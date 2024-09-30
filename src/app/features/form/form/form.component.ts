d8f9336b565b53662fab494c1153e03a246d419f
Jhay-Great <alemonai.jenef@gmail.com>
2024-09-30 17:49:46 +0000

    feat: adds form markup

--- src/app/features/form/form/form.component.ts
+++ src/app/features/form/form/form.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../../core/services/auth/auth.service';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss'
 })
-export class FormComponent {
+export class FormComponent implements OnInit {
+  form!: FormGroup;
+
+  constructor (
+    private authService: AuthService,
+    private fb: FormBuilder,
+  ) {};
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(20)]]
+    })
+  }
+
+  get email () {
+    return this.form.get('email');
+  }
+
+  get password () {
+    return this.form.get('password');
+  }
+
+  login () {
+    const formData = this.form;
+    if (formData.invalid) {
+      console.log(formData);
+      return;
+    }
+    const data = formData.value;
+    this.authService.login(data);
+  }
 
 }
