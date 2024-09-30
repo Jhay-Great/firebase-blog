e9baf6408ca6395feab9d2a500d688cd3d7a1252
Jhay-Great <alemonai.jenef@gmail.com>
2024-09-30 11:24:15 +0000

    add initial setup for firebase, import dependencies in app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,66 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import {
+  getAnalytics,
+  provideAnalytics,
+  ScreenTrackingService,
+  UserTrackingService,
+} from '@angular/fire/analytics';
+import {
+  initializeAppCheck,
+  ReCaptchaEnterpriseProvider,
+  provideAppCheck,
+} from '@angular/fire/app-check';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getFunctions, provideFunctions } from '@angular/fire/functions';
+import { getMessaging, provideMessaging } from '@angular/fire/messaging';
+import { getPerformance, providePerformance } from '@angular/fire/performance';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import {
+  getRemoteConfig,
+  provideRemoteConfig,
+} from '@angular/fire/remote-config';
+import { getVertexAI, provideVertexAI } from '@angular/fire/vertexai-preview';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'fir-blog-e8dc0',
+        appId: '1:714868888360:web:f1d231c0520ae50b2cf61d',
+        storageBucket: 'fir-blog-e8dc0.appspot.com',
+        apiKey: 'AIzaSyBJ_l5-bkeVMXdYxslK3ZNcVsIJQc83KCk',
+        authDomain: 'fir-blog-e8dc0.firebaseapp.com',
+        messagingSenderId: '714868888360',
+        measurementId: 'G-6M8CRJCPKW',
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideAnalytics(() => getAnalytics()),
+    ScreenTrackingService,
+    UserTrackingService,
+    // provideAppCheck(() => {
+    //   // TODO get a reCAPTCHA Enterprise here https://console.cloud.google.com/security/recaptcha?project=_
+    //   const provider =
+    //     new ReCaptchaEnterpriseProvider(/* reCAPTCHA Enterprise site key */);
+    //   return initializeAppCheck(undefined, {
+    //     provider,
+    //     isTokenAutoRefreshEnabled: true,
+    //   });
+    // }),
+    provideFirestore(() => getFirestore()),
+    provideDatabase(() => getDatabase()),
+    provideFunctions(() => getFunctions()),
+    provideMessaging(() => getMessaging()),
+    providePerformance(() => getPerformance()),
+    provideStorage(() => getStorage()),
+    provideRemoteConfig(() => getRemoteConfig()),
+    provideVertexAI(() => getVertexAI()),
+  ],
 };
