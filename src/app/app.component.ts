066f5ce151635bbd01060c3592294ef3630bf3bc
Jhay-Great <alemonai.jenef@gmail.com>
2024-10-03 09:57:05 +0000

    feat: adds profile markup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
 import { PostListComponent } from './pages/post-list/post-list.component';
+import { NavbarComponent } from './shared/features/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent, SignUpComponent, PostListComponent],
+  imports: [RouterOutlet, LoginComponent, SignUpComponent, NavbarComponent, PostListComponent, ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
